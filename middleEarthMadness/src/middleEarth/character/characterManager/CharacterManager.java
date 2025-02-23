c2410ea0810c50f1cc7107a27771e62b0fd20084
annamori900 <150392971+annamori900@users.noreply.github.com>
2025-02-23 12:17:44 -0600

    add updateCharacter() method to CharacterManager

--- middleEarthMadness/src/middleEarth/character/characterManager/CharacterManager.java
+++ middleEarthMadness/src/middleEarth/character/characterManager/CharacterManager.java
@@ -22,8 +22,28 @@ public class CharacterManager {
 	}
 	
 	public boolean updateCharacter(MiddleEarthCharacter character, String name, double health, double power) {
+		int change = 0;
+		// TODO this could be wrong, idk if we check if the character is in the array or not
+		for(int i = 0; i < size; i++) {
+			if (characters[i] == character) {
+				if (characters[i].getName() != name) {
+					characters[i].setName(name);
+					change = 1;
+				}
+				if (characters[i].getHealth() != health) {
+					characters[i].setHealth(health);
+					change = 1;
+				}
+				if (characters[i].getPower() != power) {
+					characters[i].setPower(power);
+					change = 1;
+				}
+			}
+		}
+		if (change == 1) {
+			return true;
+		}
 		return false;
-		
 	}
 	
 	public boolean deleteCharacter(MiddleEarthCharacter character) {
