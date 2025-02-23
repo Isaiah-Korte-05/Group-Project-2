613eb616ffb50c6f2cfb2e70cffaa5dcfef1177c
annamori900 <150392971+annamori900@users.noreply.github.com>
2025-02-23 11:31:28 -0600

    add MiddleEarthCharacter fields and methods

--- middleEarthMadness/src/middleEarth/character/MiddleEarthCharacter.java
+++ middleEarthMadness/src/middleEarth/character/MiddleEarthCharacter.java
@@ -1,5 +1,24 @@
 package middleEarth.character;
 
 public abstract class MiddleEarthCharacter {
+	
+	protected String name;
+	protected double health;
+	protected double power;
+	
+	public MiddleEarthCharacter(String name, double health, double power) {
+		this.health = health;
+		this.power = power;
+	}
+	
+	protected abstract boolean attack(MiddleEarthCharacter target);
+	
+	protected abstract String getRace();
+	
+	public void displayInfo() {
+		System.out.println("Name: " + name);
+		System.out.println("Health: " + health);
+		System.out.println("Power: " + power);
+	}
 
 }
