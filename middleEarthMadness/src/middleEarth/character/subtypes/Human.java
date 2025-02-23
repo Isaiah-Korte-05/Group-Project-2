6d23d4fd7cdd8c897442a52d3d5fb4bb956b9da4
annamori900 <150392971+annamori900@users.noreply.github.com>
2025-02-23 11:39:27 -0600

    add stubs to subtype classes

--- middleEarthMadness/src/middleEarth/character/subtypes/Human.java
+++ middleEarthMadness/src/middleEarth/character/subtypes/Human.java
@@ -4,4 +4,20 @@ import middleEarth.character.MiddleEarthCharacter;
 
 public class Human extends MiddleEarthCharacter{
 
+	public Human(String name, double health, double power) {
+		super(name, health, power);
+	}
+
+	@Override
+	protected boolean attack(MiddleEarthCharacter target) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected String getRace() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
--- middleEarthMadness/src/middleEarth/character/subtypes/Wizard.java
+++ middleEarthMadness/src/middleEarth/character/subtypes/Wizard.java
@@ -4,4 +4,20 @@ import middleEarth.character.MiddleEarthCharacter;
 
 public class Wizard extends MiddleEarthCharacter{
 
+	public Wizard(String name, double health, double power) {
+		super(name, health, power);
+	}
+
+	@Override
+	protected boolean attack(MiddleEarthCharacter target) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected String getRace() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
