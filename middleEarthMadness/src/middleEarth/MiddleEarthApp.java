387a1761961cb882e9e853c274cd1096e1446e82
Isaiah-Korte-05 <ijkmmf@gmail.com>
2025-02-24 09:25:01 -0600

    main() function
    made new main() function separate from menu

---
+++ middleEarthMadness/src/middleEarth/MiddleEarthApp.java
@@ -0,0 +1,13 @@
+package middleEarth;
+
+import middleEarth.menu.MiddleEarthAppMenu;
+
+public class MiddleEarthApp {
+
+	public static void main(String[] args) {
+
+		MiddleEarthAppMenu.menu();
+		
+	}
+
+}
