db3e842c9b1eef86b0cf7d8411c51ab3a54d9ceb
Isaiah-Korte-05 <ijkmmf@gmail.com>
2025-02-26 09:34:27 -0600

    updated package info

---
+++ middleEarthMadness/src/middleEarthApp/MiddleEarthApp.java
@@ -0,0 +1,18 @@
+package middleEarthApp;
+
+import middleEarthApp.menu.*;
+
+public class MiddleEarthApp {
+	
+	public static void main(String[] args) {
+
+		System.out.println("Starting simulation...");
+		
+		addNewChar("dwarf", "Gimli", (long)25, (long)4);
+		
+		
+		System.out.println("Exiting simulation...");
+		
+	}
+
+}
