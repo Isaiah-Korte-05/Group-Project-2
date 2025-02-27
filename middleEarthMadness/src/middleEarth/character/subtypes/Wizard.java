0a76bc8c56347dd559833c847b1136540992bcf0
annamori900 <150392971+annamori900@users.noreply.github.com>
2025-02-27 13:47:16 -0600

    add javadoc to Wizard

--- middleEarthMadness/src/middleEarth/character/subtypes/Wizard.java
+++ middleEarthMadness/src/middleEarth/character/subtypes/Wizard.java
@@ -4,10 +4,26 @@ import middleEarth.character.MiddleEarthCharacter;
 
 public class Wizard extends MiddleEarthCharacter{
 
+	/**
+	 * This is a parameterized constructor that initializes the name, health,
+	 * and power of a Wizard
+	 * @param name This parameter is a String for the name of the Wizard
+	 * @param health This parameter is a double for the health of the Wizard
+	 * @param power This parameter is a double for the power of the Wizard
+	 */
 	public Wizard(String name, double health, double power) {
 		super(name, health, power);
 	}
 
+	/**
+	 * This attacks a MiddleEarthCharacter.
+	 * Wizard has 1.5x damage against Dwarf,
+	 * 		      1.0x damage against Elf and Orc,
+	 * 	          0.0x damage against Human and Wizard
+	 * @param target This parameter is the MiddleEarthCharacter object to be attacked
+	 * @return Returns true if the attack is successful and the target's health is reduced,
+	 * 		   or false if the attack was ineffective or against a target of own kin
+	 */
 	@Override
 	public boolean attack(MiddleEarthCharacter target) {
 		if (target.getRace().equals("Dwarf")) {
@@ -23,6 +39,10 @@ public class Wizard extends MiddleEarthCharacter{
 		}
 	}
 
+	/**
+	 * This gets the race of the character
+	 * @return Returns a String of the race of the MiddleEarthCharacter
+	 */
 	@Override
 	public String getRace() {
 		return this.getClass().getSimpleName();
