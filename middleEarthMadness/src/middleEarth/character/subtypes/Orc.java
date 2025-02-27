62ca891913fb35b1bbdb488c0efe5f0f5ee43e98
annamori900 <150392971+annamori900@users.noreply.github.com>
2025-02-27 13:45:49 -0600

    add javadoc to Orc

--- middleEarthMadness/src/middleEarth/character/subtypes/Orc.java
+++ middleEarthMadness/src/middleEarth/character/subtypes/Orc.java
@@ -4,10 +4,26 @@ import middleEarth.character.MiddleEarthCharacter;
 
 public class Orc extends MiddleEarthCharacter{
 
+	/**
+	 * This is a parameterized constructor that initializes the name, health,
+	 * and power of an Orc
+	 * @param name This parameter is a String for the name of the Orc
+	 * @param health This parameter is a double for the health of the Orc
+	 * @param power This parameter is a double for the power of the Orc
+	 */
 	public Orc(String name, double health, double power) {
 		super(name, health, power);
 	}
 
+	/**
+	 * This attacks a MiddleEarthCharacter.
+	 * Orc has 1.5x damage against Human,
+	 * 		   1.0x damage against Dwarf and Wizard,
+	 * 		   0.0x damage against Elf and Orc
+	 * @param target This parameter is the MiddleEarthCharacter object to be attacked
+	 * @return Returns true if the attack is successful and the target's health is reduced,
+	 * 		   or false if the attack was ineffective or against a target of own kin
+	 */
 	@Override
 	public boolean attack(MiddleEarthCharacter target) {
 		if (target.getRace().equals("Human")) {
@@ -23,6 +39,10 @@ public class Orc extends MiddleEarthCharacter{
 		}
 	}
 
+	/**
+	 * This gets the race of the character
+	 * @return Returns a String of the race of the MiddleEarthCharacter
+	 */
 	@Override
 	public String getRace() {
 		return this.getClass().getSimpleName();
