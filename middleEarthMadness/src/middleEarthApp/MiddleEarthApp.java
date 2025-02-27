ff613333356499023a822112341333e5ad3439cf
Isaiah-Korte-05 <ijkmmf@gmail.com>
2025-02-27 11:21:40 -0600

    Basic simulation class

--- middleEarthMadness/src/middleEarthApp/MiddleEarthApp.java
+++ middleEarthMadness/src/middleEarthApp/MiddleEarthApp.java
@@ -1,18 +1,52 @@
 package middleEarthApp;
 
-import middleEarthApp.menu.*;
+import middleEarthApp.menu.MiddleEarthAppMenu;
 
 public class MiddleEarthApp {
-	
+
 	public static void main(String[] args) {
 
 		System.out.println("Starting simulation...");
 		
-		addNewChar("dwarf", "Gimli", (long)25, (long)4);
+		// Prints menu options
+		MiddleEarthAppMenu.printOptions();
 		
+		// Adds characterOne
+		MiddleEarthAppMenu.addNewChar("human", "Boromir", 14, 2);
 		
-		System.out.println("Exiting simulation...");
+		// Adds characterTwo
+		MiddleEarthAppMenu.addNewChar("elf", "Legolas", 31, 10);
 		
+		// Adds characterThree
+		MiddleEarthAppMenu.addNewChar("human", "Aragorn", 38, 5);
+		
+		// Prints all characters
+		MiddleEarthAppMenu.viewCharacters();
+		
+		// Updates character with changes
+		MiddleEarthAppMenu.updateChar("Aragorn", 50, 10);
+		
+		// Adds characterFour
+		MiddleEarthAppMenu.addNewChar("dwarf", "Gimli", 27, 3);
+		
+		// Tries to update character with no changes
+		MiddleEarthAppMenu.updateChar("Legolas", 31, 10);
+		
+		// Deletes character
+		MiddleEarthAppMenu.deleteChar("Gimli");
+				
+		// Adds characterFive
+		MiddleEarthAppMenu.addNewChar("orc", "Azog", 12, 8);
+		
+		// Prints all characters
+		MiddleEarthAppMenu.viewCharacters();
+		
+		// Initiates attack sequence
+		MiddleEarthAppMenu.attackChar();
+		
+		// Exits simulation
+		MiddleEarthAppMenu.exit();
+
 	}
 
 }
