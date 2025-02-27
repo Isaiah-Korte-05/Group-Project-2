a93414322f96fbd66eea8888b94b62934c6d4024
annamori900 <150392971+annamori900@users.noreply.github.com>
2025-02-27 13:23:24 -0600

    add javadoc to CharacterManager

--- middleEarthMadness/src/middleEarth/character/characterManager/CharacterManager.java
+++ middleEarthMadness/src/middleEarth/character/characterManager/CharacterManager.java
@@ -4,14 +4,22 @@ import middleEarth.character.MiddleEarthCharacter;
 
 public class CharacterManager {
 
-	public MiddleEarthCharacter[] characters;
-	public int size = 0;
+	// Class fields for CharacterManager
+	public MiddleEarthCharacter[] characters; // stores MiddleEarthCharacter objects
+	public int size = 0; // tracks stored characters
 	
+	/**
+	 * This is a constructor that initializes a MiddleEarthCharater array
+	 * of size 4
+	 */
 	public CharacterManager() {
 		this.characters = new MiddleEarthCharacter[4];
 	}
 	
 	//TODO how to test success
+	/**
+	 * This adds a MiddleEarthCharacter. If the array is full, doubles array.
+	 */
 	public boolean addCharacter(MiddleEarthCharacter c) {
 		
 		if(size == (characters.length - 1)) {
@@ -26,6 +34,12 @@ public class CharacterManager {
 		return true;
 	}
 	
+	/**
+	 * This retrieves a MiddleEarthCharacter by name
+	 * @param name This parameter is a String of the name of the desired MiddleEarthCharacter
+	 * @return Returns MiddleEarthCharacter object if found,
+	 * 		   or null if not found
+	 */
 	public MiddleEarthCharacter getCharacter(String name) {
 		for (int i = 0; i < size; i++) {
 			if (characters[i].getName() == name) {
@@ -35,6 +49,15 @@ public class CharacterManager {
 		return null;
 	}
 	
+	/**
+	 * This updates a MiddleEarthCharacter
+	 * @param character This parameter is the MiddleEarthCharacter object to be updated
+	 * @param name This parameter is a String of the desired updated MiddleEarthCharacter name
+	 * @param health This parameter is a double of the desired updated MiddleEarthCharacter health
+	 * @param power This parameter is a double of the desired updated MiddleEarthCharacter power
+	 * @return Returns true if a change is made to the MiddleEarthCharacter,
+	 * 		   or false if there is no change or the MiddleEarthCharacter does not exist
+	 */
 	public boolean updateCharacter(MiddleEarthCharacter character, String name, double health, double power) {
 		
 		boolean change = false;
@@ -61,6 +84,12 @@ public class CharacterManager {
 	}
 	
 	// TODO how to test success
+	/**
+	 * This deletes a MiddleEarthCharacter and shifts later elements in the array.
+	 * @param character This parameter is the MiddleEarthCharacter object to be deleted
+	 * @return Returns true if MiddleEarthCharacter is successfully deleted,
+	 * 		   or false if character not found
+	 */
 	public boolean deleteCharacter(MiddleEarthCharacter character) {
 		
 		for (int i = 0; i < size; i++) {
@@ -77,6 +106,9 @@ public class CharacterManager {
 		
 	}
 	
+	/**
+	 * This displays all characters.
+	 */
 	public void displayAllCharacters() {
 		
 		System.out.println("--------------------------------------");
